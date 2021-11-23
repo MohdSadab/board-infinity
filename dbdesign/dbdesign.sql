bb03c0a14aebc23a40ae518985dc162fb44b5bc6
MohdSadab <sadabkhan14198@gmail.com>
2021-11-23 10:49:10 +0530

    added db design file

---
+++ dbdesign/dbdesign.sql
@@ -0,0 +1,16 @@
+-- Design a database for test management
+-- functionality
+-- Question have multiple answer and multiple category
+-- Each answer belongs to only one question
+-- Each category can have more then one Question 
+
+-- Question What is MySql?
+    -- category Database CS MySql
+    -- What is C?
+        -- Programming Language CS 
+
+-- Question to option relationship also known as one to many relationship foreign key add to that table where many occurs
+-- Question to category relationship is many to many relationship here we need to create a new table for mapping
+
+
+
